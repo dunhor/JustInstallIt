817e1008542462723ccd469596e3f77162d08c2a
Duncan Horn <dunhor@microsoft.com>
2025-08-11 13:09:04 -0700

    Change cert store locations

--- JustInstallIt.ps1
+++ JustInstallIt.ps1
@@ -1,12 +1,29 @@
 
 param (
     [Parameter(Mandatory, Position = 0)]
-    [string]$AppxPath
+    [string]$AppxPath,
+
+    [Parameter(Mandatory = $false)]
+    [bool]$CreateInUserStore = $false,
+
+    [Parameter(Mandatory = $false)]
+    [bool]$InstallInUserStore = $false
 )
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
+# Determine if we're running as admin. This will affect where the certificate is installed, unless told otherwise
+$runningAsAdmin = [Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $runningAsAdmin) {
+    # If not running as admin, create in user store, even if caller explicitly said not to
+    $CreateInUserStore = $true
+}
+
+if (-not $PSBoundParameters.ContainsKey("InstallInUserStore")) {
+    $InstallInUserStore = $CreateInUserStore
+}
 
 # Extract the AppxManifest.xml so that we can get the proper subject name needed for the cert
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
 $absPath = Resolve-Path $AppxPath # OpenRead can fail if given a relative path
 $appxContents = [System.IO.Compression.ZipFile]::OpenRead($absPath)
 $entry = $appxContents.Entries | Where-Object { $_.FullName -eq "AppxManifest.xml" }
@@ -22,16 +39,19 @@ Write-Host "Package has publisher: $appxSubject"
 Remove-Item $manifestPath
 $appxContents.Dispose()
 
-# Cert information
-$certFriendlyName = "JustInstallIt Signing Cert"
-$userStore = "Cert:\CurrentUser\My"
+# Cert information. If we're running as admin, prefer to locate/create the cert in the LocalMachine store, unless explicitly
+# told by the user to use the CurrentUser store. This is somewhat of a security thing; we want to restrict access to the
+# private key, so not just any medium IL application can use it.
+$certFriendlyName = "JustInstallIt Package Signing Cert"
+$certCreateStore = if ($CreateInUserStore) { "Cert:\CurrentUser\My" } else { "Cert:\LocalMachine\My" }
+$certInstallStore = if ($InstallInUserStore) { "Cert:\CurrentUser\Root" } else { "Cert:\LocalMachine\TrustedPeople" }
 
 # Check for existing certificate
-$cert = Get-ChildItem -Path $userStore | Where-Object { ($_.Subject -eq $appxSubject) -and ($_.FriendlyName -eq $certFriendlyName) }
+$cert = Get-ChildItem -Path $certCreateStore | Where-Object { ($_.Subject -eq $appxSubject) -and ($_.FriendlyName -eq $certFriendlyName) }
 
 if (-not $cert) {
     Write-Host "Creating new self-signed certificate: $certFriendlyName"
-    $cert = New-SelfSignedCertificate -Type Custom -KeyUsage DigitalSignature -CertStoreLocation $userStore `
+    $cert = New-SelfSignedCertificate -Type Custom -KeyUsage DigitalSignature -CertStoreLocation $certCreateStore `
         -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}") -Subject $appxSubject `
         -FriendlyName $certFriendlyName
 }
@@ -50,27 +70,25 @@ $securePfxPassword = ConvertTo-SecureString -String $pfxPassword -Force -AsPlain
 $pfxPath = [System.IO.Path]::GetTempFileName() + ".pfx"
 Export-PfxCertificate -Cert $cert -FilePath $pfxPath -Password $securePfxPassword | Out-Null
 
-# By default, try and put the cert in the Local Computer Trusted People store. This requires admin, so fall back to the
-# Current User Root store, which seems to sometimes work, though it's unclear when or why
-$compTrustStore = "Cert:\LocalMachine\TrustedPeople"
-$userTrustStore = "Cert:\CurrentUser\Root"
-
-# Check to see if the cert is already in the Computer's store
-$compCert = Get-ChildItem "$compTrustStore\$($cert.Thumbprint)" -ErrorAction SilentlyContinue
+# Install the cert if it hasn't been installed yet
+$compCert = Get-ChildItem "$certInstallStore\$($cert.Thumbprint)" -ErrorAction SilentlyContinue
 if (-not $compCert) {
-    try {
-        Write-Host "Importing certificate to the Local Computer's Trusted People store..."
-        Import-PfxCertificate -FilePath $pfxPath -CertStoreLocation $compTrustStore -Password $securePfxPassword -ErrorAction Stop | Out-Null
-        Write-Host "Import successful"
-    } catch {
-        Write-Warning "Import into the Local Computer's Trusted People store failed."
-        Write-Warning "NOTE: If the package is not installable after this step, you may need to re-run this script as admin or install the certificate manually."
-        $userCert = Get-ChildItem "$userTrustStore\$($cert.Thumbprint)" -ErrorAction SilentlyContinue
-        if (-not $userCert) {
-            Write-Host "Falling back to Current User's Root store..."
-            Import-PfxCertificate -FilePath $pfxPath -CertStoreLocation $userTrustStore -Password $securePfxPassword | Out-Null
+    Write-Host "Importing certificate to $certInstallStore..."
+
+    # NOTE: For some reason, installing into the user's Root store seems to _never_ work, however installing to the user's
+    # root store appears to _sometimes_ work. It's not clear under which circumstances this will work, so warn the user
+    # that they may need to run as admin
+    if ($InstallInUserStore) {
+        Write-Warning "Installing the certificate to the user's Root store may or may not work"
+        if ($runningAsAdmin) {
+            Write-Warning "If the application fails to install, try running without 'InstallInUserStore'"
+        } else {
+            Write-Warning "If the application fails to install, try running as admin"
         }
     }
+
+    Import-PfxCertificate -FilePath $pfxPath -CertStoreLocation $certInstallStore -Password $securePfxPassword -ErrorAction Stop | Out-Null
+    Write-Host "Import successful"
 }
 
 # Find signtool.exe (assumes Windows SDK is installed and in PATH)
